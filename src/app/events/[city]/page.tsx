f40c9003ccfd9966ab5e6f74bd807bd493368e2c
Bilal Khan <bilal.khan@live.com>
2024-08-10 09:55:23 +1000

    retrieve params for city pages.

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,11 +1,26 @@
 import Heading from "@/components/Heading";
 import HeroContainer from "@/components/HeroContainer";
 
-const EventsPage = ({ params }: { params: { city: string } }) => {
+type EventPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+const EventsPage = ({ params }: EventPageProps) => {
+  const { city } = params;
+  function capitalizeFirstWordAndCity(text: string, city: string): string {
+    const words = text.split(" ");
+    if (words.length > 0) {
+      words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+    }
+    const capitalizedCity = city.charAt(0).toUpperCase() + city.slice(1);
+    return `${words.join(" ")} in ${capitalizedCity}`;
+  }
   return (
     <>
       <HeroContainer>
-        <Heading>{params.city}</Heading>
+        <Heading>{capitalizeFirstWordAndCity("events", city)}</Heading>
       </HeroContainer>
     </>
   );
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Heading({ children }: { children: React.ReactNode }) {
   return (
-    <div className="text-3xl lg:text-6xl tracking-tight  font-semibold">
+    <div className="text-3xl lg:text-6xl tracking-tight  font-semibold ">
       {children}
     </div>
   );
